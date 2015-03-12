12dfc9ff0c1be93d1483409629f2a9cf954971a1
balakumaranmuthurajan <muthurajanbalakumaran@gmail.com>
2015-03-12 15:22:00 +0530

    few validations
    waorked with few toast

--- app/src/main/java/com/littleo/netpda/MainActivity.java
+++ app/src/main/java/com/littleo/netpda/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,7 +21,19 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(new Intent(MainActivity.this,Signup.class));
             }
         });
-
+        Button bt2 =(Button) findViewById(R.id.login);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email;
+                String pass;
+                EditText et1 = (EditText) findViewById(R.id.email);
+                EditText et2 = (EditText) findViewById(R.id.pass);
+                email = et1.getText().toString();
+                pass = et2.getText().toString();
+                Toast.makeText(MainActivity.this,email,Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this,pass,Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
 }
